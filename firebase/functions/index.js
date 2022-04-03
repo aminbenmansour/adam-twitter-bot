cf53e29a7fdbb2a012a6967c942f08fb249049d6
aminbenmansour <amin.benmansour.10@gmail.com>
2022-04-03 17:49:16 +0100

    dotenv

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,7 +1,17 @@
 const functions = require("firebase-functions");
 
+require('dotenv').config();
+
 const admin = require("firebase-admin");
 admin.initializeApp();
+
+const dbRef = admin.firestore().doc("tokens/demo");
+
+const TwitterApi = require("twitter-api-v2").default;
+const twitterClient = new TwitterApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+});
 
 // step 1
 exports.auth = functions.https.onRequest((request, response) => {});
